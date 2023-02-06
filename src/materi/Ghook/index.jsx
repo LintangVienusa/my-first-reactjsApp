d778c54990d514910a721d8a3cad26b051af3e2a
LintangVienusa <vienusa6661@gmail.com>
2023-02-07 01:19:19 +0700

    learn how to use identity, useCallback, and custom hook

--- src/materi/Ghook/index.jsx
+++ src/materi/Ghook/index.jsx
@@ -1,12 +1,35 @@
 // import Counter from "./pembahasan/Counter";
+// import Effect from "./pembahasan/Effect";
 
-import Effect from "./pembahasan/Effect";
+import { useState } from "react";
+import { useToggle } from "./pembahasan/hook/useToggle";
 
 const Hooks = () => {
+
+    let [label, setLabel] = useState('ON');
+    let [switches, setSwitches] = useToggle();
+
+    const style = {
+        background: switches ? 'yellow' : 'black',
+        textAlign: 'center',
+        height: '400px'
+    }
+
+    const handleSwitches = () => {
+        setSwitches(!switches);
+        setLabel(e => {
+            if(e === 'ON'){
+                return 'OFF'
+            }else{
+                return 'ON'
+            }
+        })
+
+    }
+
     return(
-        <div>
-            <Effect />
-            {/* <Counter /> */}
+        <div style={style}>
+            <button onClick={handleSwitches}> {label} </button>
         </div>
     )
 }
